c14bc4be7561943858533cb1d2fbde95c6d4c3ef
Kevin Firko <kfirko@bitcurve.com>
2025-04-26 00:42:05 -0400

    feat: revised root w/ theme term + new layout

--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,26 @@
 import type React from 'react'
-import { isRouteErrorResponse, Links, Meta, Outlet, Scripts, ScrollRestoration, useRouteLoaderData } from 'react-router'
+import {
+  href,
+  isRouteErrorResponse,
+  Link,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useRouteLoaderData,
+} from 'react-router'
 
 import type { Route } from './+types/root'
 
-import './styles/app.css'
-import { getColorScheme } from './theme/color-scheme-cookie'
+import '@/styles/app.css'
+import { getTheme } from '@/theme/theme.server'
+import { ThemeMenu } from '@/theme/theme.components'
+import { Logo } from '@/components/layout/logo'
 
 export async function loader({ request }: Route.LoaderArgs) {
-  const colorScheme = await getColorScheme(request)
-  return { colorScheme }
+  const theme = await getTheme(request)
+  return { theme }
 }
 
 export const links: Route.LinksFunction = () => [
@@ -26,8 +38,9 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }): React.JSX.Element {
   const loaderData = useRouteLoaderData<typeof loader>('root')
+
   return (
-    <html lang="en" className={loaderData?.colorScheme ?? 'system'}>
+    <html lang="en" dir="ltr" className={loaderData?.theme ?? 'system'}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -43,8 +56,44 @@ export function Layout({ children }: { children: React.ReactNode }): React.JSX.E
   )
 }
 
+function capitalizeFirst(string: string): string {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
 export default function App(): React.JSX.Element {
-  return <Outlet />
+  return (
+    <div className="flex flex-col min-h-svh">
+      <header className="bg-muted dark:bg-muted/50">
+        <div className="container flex items-center justify-center mx-auto py-4">
+          <Link to={href('/')} viewTransition className="flex-1">
+            <Logo className="h-6" />
+          </Link>
+          <ul className="flex items-center gap-8 text-sm">
+            {[href('/'), href('/demo'), href('/about')].map((link) => (
+              <li key={link}>
+                <Link to={link} viewTransition className="font-semibold hover:underline">
+                  {link === '/' ? 'Home' : capitalizeFirst(link.replace('/', ''))}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="flex items-center justify-end flex-1">
+            <ThemeMenu />
+          </div>
+        </div>
+      </header>
+      <main className="flex-1">
+        <div className="container mx-auto py-8 sm:py-16">
+          <Outlet />
+        </div>
+      </main>
+      <footer className="bg-muted dark:bg-muted/50 text-sm">
+        <div className="container mx-auto py-4">
+          <p className="text-center">&copy; React Router</p>
+        </div>
+      </footer>
+    </div>
+  )
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps): React.JSX.Element {
