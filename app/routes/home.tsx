04eb45e208ea77b8c16caf13c9f6174af878036a
Kevin Firko <kfirko@bitcurve.com>
2025-04-26 00:51:15 -0400

    feat: add light/dark to theme switch so you can see it

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -10,8 +10,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export async function action({ request }: Route.ActionArgs) {
-  let formData = await request.formData();
-  let colorScheme = schema.parse(formData.get("color-scheme"));
+  const formData = await request.formData();
+  const colorScheme = schema.parse(formData.get("color-scheme"));
+
   return data(null, {
     headers: { "Set-Cookie": await setColorScheme(colorScheme) },
   });
@@ -21,13 +22,13 @@ export default function Home() {
   return (
     <Form navigate={false} method="POST" className="p-10">
       <div className="flex items-center justify-center gap-4">
-        <button type="submit" name="color-scheme" value="dark">
+        <button type="submit" name="color-scheme" value="dark" className="text-gray-950 dark:text-white">
           Dark
         </button>
-        <button type="submit" name="color-scheme" value="light">
+        <button type="submit" name="color-scheme" value="light" className="text-gray-950 dark:text-white">
           Light
         </button>
-        <button type="submit" name="color-scheme" value="system">
+        <button type="submit" name="color-scheme" value="system" className="text-gray-950 dark:text-white">
           System
         </button>
       </div>
