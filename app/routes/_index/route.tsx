52f1c7e8d2cc41c116cb331b06397fd0a6f43300
Kevin Firko <kfirko@bitcurve.com>
2025-04-26 21:33:43 -0400

    feat: add theme debug to landing

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -3,6 +3,8 @@ import type React from 'react'
 import { HeadMeta } from '@/components/layout/head-meta'
 import { Button } from '@/components/ui/button'
 import { Link } from 'react-router'
+import { cn } from '@/lib/style'
+import { MoonIcon, SunIcon } from 'lucide-react'
 
 export default function IndexRoute(): React.JSX.Element {
   const handleClick = () => {
@@ -26,24 +28,61 @@ export default function IndexRoute(): React.JSX.Element {
             Outline Button
           </Button>
         </div>
-        <h2 className="text-2xl font-bold">Heads Up</h2>
         <p>
           This repo demonstrates an issue with CSS specificity regarding the system theme and tailwind v4 + shadcn v2.
         </p>
+        <h2 className="text-2xl font-bold">Theme Debug</h2>
         <p>
-          Everything else works: use <code>remix-themes</code>, <code>next-themes</code> (which is compatible with
-          react-router), or a client-hints implementation to sidestep the issue.
+          Tailwind-first apps should respect the <span className="font-medium">Tailwind Theme</span>.
         </p>
-        <h2 className="text-2xl font-bold">Get the Code</h2>
+        <div className="grid gap-2">
+          <div className="flex gap-6 justify-between items-center p-2 rounded-md bg-muted text-sm">
+            <div className="flex flex-col">
+              <span>Tailwind Theme</span>
+              <span className="text-xs text-neutral-500">
+                <code>dark:*</code>
+              </span>
+            </div>
+            <code className="p-2 rounded-md text-xs bg-white text-neutral-900 dark:bg-neutral-950 dark:text-neutral-50 border border-neutral-500/50">
+              <span className="inline dark:hidden">light</span>
+              <span className="hidden dark:inline">dark</span>
+            </code>
+          </div>
+          <div className="flex gap-6 justify-between items-center p-2 rounded-md bg-muted text-sm">
+            <div className="flex flex-col">
+              <span>Preferred Color Scheme</span>
+              <span className="text-xs text-neutral-500">
+                <code>[@media(prefers-color-scheme:_)]:*</code>
+              </span>
+            </div>
+            <code className="p-2 rounded-md text-xs bg-white text-neutral-900 dark:bg-neutral-950 dark:text-neutral-50 border border-neutral-500/50">
+              <span className="hidden [@media(prefers-color-scheme:light)]:flex">light</span>
+              <span className="hidden [@media(prefers-color-scheme:dark)]:flex">dark</span>
+            </code>
+          </div>
+        </div>
+        <h2 className="text-2xl font-bold">Theme Variables</h2>
         <p>
-          <Link
-            to="https://github.com/firxworx/rr-shadcn-theme"
-            target="_blank"
-            rel="noreferrer"
-            className="text-sky-700 dark:text-sky-400 no-underline hover:underline"
-          >
-            https://github.com/firxworx/rr-shadcn-theme
-          </Link>
+          shadcn/ui and other Tailwind-first libraries use CSS variables to define colors in <code>:root</code> with
+          dark theme override under <code>.dark</code>:
+        </p>
+        <div className="grid gap-2 text-sm">
+          <div className="text-foreground bg-muted p-2 rounded-md">
+            This box is styled using shadcn color utilities.
+          </div>
+        </div>
+        <p>
+          shadcn/ui <code>&lt;Button&gt;</code>'s <em>outline</em> variant uses both tailwind <code>dark:</code> in its{' '}
+          <code>className</code> as well as shadcn color utilies with definitions tied to CSS variables with different{' '}
+          <code>:root</code> vs. <code>.dark</code>.
+        </p>
+        <p>This makes a good test candidate for debugging styles with system theme:</p>
+        <Button variant="outline" className="w-fit">
+          Demo Button
+        </Button>
+        <p>
+          The button should appear strictly identical with no differences between explicit light/dark theme and fallback
+          to browser's system light/dark preference.
         </p>
       </section>
     </>
